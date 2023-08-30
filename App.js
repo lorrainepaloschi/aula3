579e206b74dfe0730df01a8f447e97d78130692c
Lorraine Paloschi <lorraine.paloschi@gmail.com>
2023-08-29 21:56:10 -0300

    exercicio 4

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Hello from './Hello';
 import Ola from './Ola';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Ola/>
-      <Hello/>
-      <Text>Open up App js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Ola/>
+//       <Hello/>
+//       <Text>Open up App js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Ola/>
+        <Hello/>
+        <Text>Open up App js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
