5c81d3dc82e1d6306338efed590aa2c73c8f6b99
Lorraine Vieira <lorraine.paloschi@gmail.com>
2023-10-31 00:38:27 -0300

    init backend

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
